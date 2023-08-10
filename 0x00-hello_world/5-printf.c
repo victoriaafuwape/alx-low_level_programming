2b4e5d7262f312c1f689dd23690c3b9b6b7a2fa0
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-08-10 14:59:52 -0700

    Mandatory

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/* What's Programming? */
+#include <stdio.h> /* preprocessor directive */
+
+/**
+ * main - entry point for the program
+ * printf - outputs a string
+ * Return: 0 indicates successful execution
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+/* Size of days types */
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * printf - outputs lines of strings
+ *
+ * Return: 0 indicates successful execution
+ */
+
+main(void)
+{
+	char
+	int
+	float
+	double
+
+	printf("Size of a char: %d-byte\n", sizeof (char));
+	printf("Size of an int: %d-byte\n", sizeof (int));
+	printf("Size of a long int: %d-byte\n", sizeof (int));
+	printf("Size of a long long int: %d-byte\n", sizeof (double));
+	printf("Size of a float: %d-byte\n", sizeof (float));
+	return (0);
+}
