b3a02a8b5ba33a395c4e77565b266fa5a44de8c2
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-08-10 15:09:52 -0700

    Mandatory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,10 @@
 
 int main(void)
 {
-	char
-	int
-	float
-	double
-
-	printf("Size of a char: %d-byte\n", sizeof (char));
-	printf("Size of an int: %d-byte\n", sizeof (int));
-	printf("Size of a long int: %d-byte\n", sizeof (int));
-	printf("Size of a long long int: %d-byte\n", sizeof (double));
-	printf("Size of a float: %d-byte\n", sizeof (float));
+	printf("Size of a char: %d-byte(s\)\n", sizeof (char));
+	printf("Size of an int: %d-byte(s\)\n", sizeof (int));
+	printf("Size of a long int: %d-byte(s\)\n", sizeof (long int));
+	printf("Size of a long long int: %d-byte(s\)\n", sizeof (long long int));
+	printf("Size of a float: %d-byte(s\)\n", sizeof (float));
 	return (0);
 }
