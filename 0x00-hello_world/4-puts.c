3a9d4ae726c67de15dcd3112451a4ea81e67d070
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-08-10 13:05:10 -0700

    Mandatory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/* What's Programming? */
+#include <stdio.h> /* preprocessor directive */
+
+/**
+ * main - entry point for the program
+ * puts - function outputs a string and appends a newline
+ * Return: the value 0 indicates successful execution
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
