156cecb292e21f844de5df7b20b4f6a690931485
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-08-17 07:52:37 -0700

    Mandatory

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* positive_or_negative - tests function that prints positive or negative integer
+* Return: 0
+*/
+
+int positive_or_negative(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
