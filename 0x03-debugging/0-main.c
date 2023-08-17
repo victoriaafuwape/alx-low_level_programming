eb22f47ec48866160d4f6cf1f87334a90f709d6c
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-08-17 07:59:27 -0700

    Testing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
-* positive_or_negative - tests function that prints positive or negative integer
+* positive_or_negative - function name
 * Return: 0
 */
 
 int positive_or_negative(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 98;
 
-        return (0);
+	positive_or_negative(i);
+
+	return (0);
 }
