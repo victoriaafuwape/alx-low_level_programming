a72cf6b46a1b6ad148f72c62d377cf3ac8309733
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-08-17 13:33:34 -0700

    Advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,48 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
- * @n: The number to start printing from
- *
- * Return: Always 0 (Success)
- */
-
-void print_to_98(int n)
-{
-	if (n <= 98)
-	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d\n", n);
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d\n", n);
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
-}
-
-/**
  * print_times_table - Prints the n times table starting from 0
+ *
  * @n: The number to generate the times table for
  */
 
@@ -58,11 +18,4 @@ void print_times_table(int n)
 	{
 		printf("%d x %d = %d\n", n, i, n * i);
 	}
-}
-
-int main(void)
-{
-	print_to_98(5);
-	print_times_table(7);
-	return (0);
 }
