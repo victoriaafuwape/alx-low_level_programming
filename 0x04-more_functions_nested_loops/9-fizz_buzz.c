6a84c8dcaeb45b1a246257a94fc89d44ca618568
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-08-18 16:41:34 -0700

    Mandatory

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the number 1 - 100
+ * Fizz for multiples of 3 and Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
