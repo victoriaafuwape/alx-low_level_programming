8252972fe92a1b4b68f809f5b9cf4b936d472463
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-09-01 09:13:31 -0700

    Correction

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -2,5 +2,10 @@
 
 for file in "/root/alx-low_level_programming/0x09-static_libraries/"*.c
 do
-    gcc -c "$file"
+    gcc -Wall -Werror -Wextra -pedantic -c "$file"
+done
+
+for file in "/root/alx-low_level_programming/0x09-static_libraries/"*.o
+do
+    ar rc liball.a "$file"
 done
