a366a795b1a743edcebbda2b511c084ac879c5cb
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-09-04 12:11:33 -0700

    Mandatory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,46 +17,33 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 	char *result;
 
-	if (s1 == 0 || s1 == 0)
-	{
+	if (s1 == 0)
 		s1 = "";
+	if (s1 == 0)
 		s2 = "";
-	}
-
 	while (s1[length] != 0)
-	{
 		length++;
-	}
-
 	while (s2[v] != 0)
 	{
 		v++;
 		length++;
 	}
-
 	cat = (char *)malloc(sizeof(char) * (length + 1));
 	result = cat;
-
 	if (cat == 0)
-	{
 		return (0);
-	}
-
 	while (*s1 != 0)
 	{
 		*cat = *s1;
 		s1++;
 		cat++;
 	}
-
 	while (*s2 != 0)
 	{
 		*cat = *s2;
 		s2++;
 		cat++;
 	}
-
 	*cat = 0;
-
 	return (result);
 }
