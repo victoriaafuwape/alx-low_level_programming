adc111cf7d1b7f533c437b5fb2355d931f47c6e1
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-09-11 10:56:14 -0700

    Fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,5 +22,5 @@ void print_dog(struct dog *d)
 	{
 		d-> owner = "(nil)";
 	}
-	printf("NName: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
