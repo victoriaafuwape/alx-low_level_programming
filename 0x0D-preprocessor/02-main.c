8f29c4ee0699c47912051bc8c7254bb8bfde8892
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-09-11 04:52:26 -0700

    Mandatory

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - program prints name of source file
+ * @void: takes no argument
+ *
+ * Return: 0 to indicate success
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
