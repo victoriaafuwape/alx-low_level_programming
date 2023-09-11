431cbe9dbd5f41d259c9c74d993f5aa775e0894e
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-09-11 12:20:29 -0700

    Fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -65,8 +65,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
 		return (NULL);
-
+	}
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
