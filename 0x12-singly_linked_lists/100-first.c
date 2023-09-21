dc08d98fcbe20a0ca758b42424d0eef99f3f2ac7
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-09-21 10:46:18 -0700

    Advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+/**
+ * premain - Prints a string before the main function is executed
+ *
+ * Return: Void.
+ */
+void premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -31,6 +31,8 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
+void premain(void) __attribute__ ((constructor));
+
 int _putchar(char c);
 
 
